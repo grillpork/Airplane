4a5762ef7b706ebde1bfa910c85b03c2bf088710
Tripop Saibut <tripopsaibut@gmail.com>
2025-01-31 22:55:15 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import FooterBar from "./components/FooterBar";
 import HeroBar from "./components/HeroBar";
 import NavBar from "./components/NavBar";
-import { motion } from "motion/react"
 
 function App() {
   return (
